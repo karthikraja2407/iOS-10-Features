ffd02e15a796b02e4acc0f842c1149109ba7d750
karthik raja raghunath <karthiktheraja@gmail.com>
2016-06-30 12:28:21 -0500

    Created project to explore iOS 10 features.

---
+++ iOS10Explore/iOS10Explore/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOS10Explore
+//
+//  Created by karthik on 6/30/16.
+//  Copyright © 2016 Verizon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOS10Explore/iOS10ExploreTests/iOS10ExploreTests.m
@@ -0,0 +1,39 @@
+//
+//  iOS10ExploreTests.m
+//  iOS10ExploreTests
+//
+//  Created by karthik on 6/30/16.
+//  Copyright © 2016 Verizon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS10ExploreTests : XCTestCase
+
+@end
+
+@implementation iOS10ExploreTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
